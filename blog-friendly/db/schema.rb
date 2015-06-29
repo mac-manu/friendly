703a16317fd0a3b6a44be9b067602ee99d368bce
Mac-Manu <manu@mac-manu.com>
2015-06-29 19:37:14 +0200

    antes de callbacks

--- blog-friendly/db/schema.rb
+++ blog-friendly/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629173327) do
+ActiveRecord::Schema.define(version: 20150629173453) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
