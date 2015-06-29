f3aa7fb4d0676c8e8abb0b96c5a03021450ca1e9
Mac-Manu <manu@mac-manu.com>
2015-06-29 19:45:03 +0200

    callbacks ok

--- blog-friendly/app/models/post.rb
+++ blog-friendly/app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+	#callbacks
+	 after_create :update_slug
+	 before_update :assign_slug
+	
+
+	#redefinimos el metodo to_param
+	 def to_param
+	     slug
+	 end
+
+	#metodos privados.
+	private
+
+	def assign_slug
+	    self.slug = "#{ title.parameterize }"
+	end
+
+	def update_slug
+	    update_attributes slug: assign_slug
+	end
 end
